64effc8b444b55c1b880330402c44640be58f10f
Suchit Krishna <skri0010@student.monash.edu>
2024-02-04 03:13:46 +0800

    on hover effect added for skills

--- Web-personal/src/components/Skills/SkillCard.tsx
+++ Web-personal/src/components/Skills/SkillCard.tsx
@@ -1,26 +1,39 @@
-import './Skills.scss'
+import "./Skills.scss";
 
 type SkillProps = {
-    skills: {
-        title: string,
-        image: string
-    }[]
+  skills: {
+    title: string;
+    image: string;
+  }[];
 };
 
 function SkillCard(props: SkillProps) {
-    return (
-        <div className='skills-grid'>
-            {
-                props.skills.map(skill => {
-                    return (
-                    <div className='skill-card item'>
-                        <div className='skill-img'><img src={skill.image} loading='lazy'></img></div>
-                        <div className='skill-text'> <span className='text'>{skill.title}</span> </div>
-                    </div>)
-                })
-            }
-        </div>
-    )
+  return (
+    <div className="skills-grid">
+      {props.skills.map((skill) => {
+        return (
+          <div className="skill-card item">
+            <div className="skill-img">
+              <img src={skill.image} loading="lazy"></img>
+            </div>
+            <div className="skill-text">
+              {" "}
+              <span className="text">{skill.title}</span>{" "}
+            </div>
+            <div className="card__inner">
+              <h2>GeeksforGeeks</h2>
+
+              <p>
+                A Computer Science portal for geeks. It contains well written,
+                well thought and well explained computer science and programming
+                articles, quizzes and ...
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default SkillCard
+export default SkillCard;
